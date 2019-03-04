0f144023610469150f165bbc222b9d4262e317f8
iwauo <iwauo.tajima@gmail.com>
2019-03-04 18:57:45 +0900

    add the project initializing script

---
+++ init.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+set -e
+
+BUILD_GRADLE=./build.gradle.kts
+BUILD_SETTINGS=./settings.gradle.kts
+
+set -x
+
+cat > $BUILD_GRADLE <<'END_OF_FILE'
+plugins {
+    java
+    maven
+    id("laplacian.generator") version "1.0.0"
+}
+repositories {
+    maven(url = "../mvn-repo/")
+    maven(url = "https://raw.github.com/nabla-squared/mvn-repo/master/")
+    jcenter()
+}
+END_OF_FILE
+
+cat > $BUILD_SETTINGS <<'END_OF_FILE'
+pluginManagement {
+    repositories {
+		maven(url = "../mvn-repo/")
+        maven(url = "https://raw.github.com/nabla-squared/mvn-repo/master/")
+        gradlePluginPortal()
+        jcenter()
+    }
+}
+END_OF_FILE
+
+gradle laplacianModule --stacktrace
+
