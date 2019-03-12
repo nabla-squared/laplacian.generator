cd07b951f23b9886c42649b587fdf28c74268857
iwauo <iwauo.tajima@gmail.com>
2019-03-12 10:30:53 +0900

    turn the class into an interface

--- src/main/kotlin/laplacian/gradle/task/generate/ModelEntryResolver.kt
+++ src/main/kotlin/laplacian/gradle/task/generate/ModelEntryResolver.kt
@@ -2,7 +2,7 @@ package laplacian.gradle.task.generate
 
 import laplacian.util.RecordList
 
-class ModelEntryResolver(
-    val resolves: (key: String, model: Map<String, RecordList>) -> Boolean,
-    val resolve: (key: String, model: Map<String, RecordList>, context: ExecutionContext) -> Any?
-)
+interface ModelEntryResolver {
+    fun resolves(key: String, model: Map<String, RecordList>): Boolean
+    fun resolve(key: String, model: Map<String, RecordList>, context: ExecutionContext): Any?
+}
