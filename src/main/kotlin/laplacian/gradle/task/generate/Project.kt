fac358822c74e08864c92cd1a851487b8b619a05
iwauo <iwauo.tajima@gmail.com>
2019-03-24 22:52:32 +0900

    add the module type of "plugin"

--- src/main/kotlin/laplacian/gradle/task/generate/Project.kt
+++ src/main/kotlin/laplacian/gradle/task/generate/Project.kt
@@ -13,6 +13,10 @@ class Project(
                 listOfNotNull(
                     required("group"), required("name"), get("subname")
                 ).map{ it.toString().lowerUnderscorize() }.joinToString(".")
+    val plugins: List<Module>
+        get() = model
+               .getList<Map<String, Any?>>("plugins", emptyList())
+               .map{ Module(it + ("type" to "plugin")) }
 
     val models: List<Module>
         get() = model
