9e4e6c33aea99c95f40aeb164119277386672ee2
iwauo <iwauo.tajima@gmail.com>
2019-04-17 03:13:41 +0900

    fix the repository path

--- init.sh
+++ init.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-
 BUILD_GRADLE=./build.gradle.kts
 BUILD_SETTINGS=./settings.gradle.kts
-HOST=https://raw.githubusercontent.com/nabla-squared/laplacian.generator/master
+REPO=https://github.com/nabla-squared/laplacian.generator/raw/master
 GRADLEW_DIR=gradle/wrapper
 
-#set -x
+set -x
 
-cat > $BUILD_GRADLE <<'END_OF_FILE'
+cat > $BUILD_GRADLE <<END_OF_FILE
 plugins {
     java
     maven
@@ -17,16 +16,16 @@ plugins {
 }
 repositories {
     maven(url = "../mvn-repo/")
-    maven(url = "https://raw.github.com/nabla-squared/mvn-repo/master/")
+    maven(url = "$REPO")
     jcenter()
 }
 END_OF_FILE
 
-cat > $BUILD_SETTINGS <<'END_OF_FILE'
+cat > $BUILD_SETTINGS <<END_OF_FILE
 pluginManagement {
     repositories {
 		maven(url = "../mvn-repo/")
-        maven(url = "https://raw.github.com/nabla-squared/mvn-repo/master/")
+        maven(url = "$REPO")
         gradlePluginPortal()
         jcenter()
     }
@@ -44,9 +43,9 @@ END_OF_FILE
 
 [ ! -f gradlew ] \
 && mkdir -p $GRADLEW_DIR \
-&& curl -o gradlew $HOST/gradlew \
-&& curl -o $GRADLEW_DIR/gradle-wrapper.jar $HOST/$GRADLE_DIR/gradle-wrapper.jar \
-&& curl -o $GRADLEW_DIR/gradle-wrapper.properties $HOST/$GRADLE_DIR/gradle-wrapper.properties \
+&& curl -Ls -o gradlew $REPO/gradlew \
+&& curl -Ls -o $GRADLEW_DIR/gradle-wrapper.jar $REPO/$GRADLEW_DIR/gradle-wrapper.jar \
+&& curl -Ls -o $GRADLEW_DIR/gradle-wrapper.properties $REPO/$GRADLEW_DIR/gradle-wrapper.properties \
 && chmod 755 gradlew
 
 ./gradlew lM --stacktrace
