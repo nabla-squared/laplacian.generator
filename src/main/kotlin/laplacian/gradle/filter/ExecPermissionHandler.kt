a631a112a2495bddc8401f6b11c38814239fef9b
iwauo <iwauo.tajima@gmail.com>
2019-04-26 02:43:01 +0900

    fix a bug that exec permission is not given if the script is generated by a template

--- src/main/kotlin/laplacian/gradle/filter/ExecPermissionHandler.kt
+++ src/main/kotlin/laplacian/gradle/filter/ExecPermissionHandler.kt
@@ -8,8 +8,7 @@ import java.io.Reader
 class ExecPermissionHandler : FileCopyHandler {
 
     override fun handle(details: FileCopyDetails, context: ExecutionContext): Boolean {
-        val template = context.currentTemplate
-        if (template.canExecute() || template.name.endsWith(".sh")) {
+        if (context.currentTemplate.canExecute() || details.name.endsWith(".sh")) {
             details.mode = 0b111_101_101 // 755
         }
         return false // This handler does not affect the content of files
