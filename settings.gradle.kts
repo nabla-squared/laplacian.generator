da3f7182a6317661bad9a4c6287ce84398d110f6
iwauo <iwauo.tajima@gmail.com>
2020-03-18 22:12:48 +0900

    update kotlin/gradle version

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "laplacian.generator"
-pluginManagement {
-    repositories {
-        maven(url = "../mvn-repo/")
-        maven(url = "https://raw.github.com/nabla-squared/mvn-repo/master/")
-        gradlePluginPortal()
-        jcenter()
-    }
-}
